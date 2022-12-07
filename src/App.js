09734b7447e263f3c370550d237578178e9ef391
Naizy <godfrednai@gmail.com>
2022-12-07 13:24:30 +0000

    editing photo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Joke from './components/Joke';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from './components/nav';
 import Main from './components/Main'
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,9 @@ import '../App.css';
 export default function Main(){
     return(
         <div className="main-body">
-            <img src={photo}  />
+            <img src={photo} alt="logo" />      
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
         </div>
     )
 }
